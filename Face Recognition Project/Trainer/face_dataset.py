d60a225dc3e9fef5ea0a4e27d31fde68c0b4edcf
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-05-25 22:02:02 +0530

    A Successful implementation of Face Training And Detection Algorithm.

---
+++ Face Recognition Project/Trainer/face_dataset.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+cam=cv2.VideoCapture(0)
+cam.set(3,640)
+cam.set(4,480)
+face_detector=cv2.CascadeClassifier('C:\\Users\\Radha\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+face_id=input('\n Enter User Id And Press Enter')
+print("\n [INFO] Intitating Face Capture")
+count=0
+while(True):
+    ret, img=cam.read()
+    # img = cv2.imread(img_path)
+    gray=cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+    faces=face_detector.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        count+=1
+        cv2.imwrite("dataset/user."+str(face_id)+'.'+str(count)+".jpg",gray[y:y+h,x:x+w])
+        cv2.imshow('image',img)
+    k = cv2.waitKey(60) & 0xff
+    if k==27:
+        break
+    elif count>=100:
+        break       
+print("\n [INFO] Exiting Program")
+cam.release()
+cv2.destroyAllWindows()
